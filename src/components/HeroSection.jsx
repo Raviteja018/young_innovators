51502f93c167aff6593f91b48955a215e8b7c4ef
raviteja018 <batturaviteja18@gmail.com>
2025-05-24 11:38:19 +0530

    final

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { INNOVATOR_IMG } from "../../utils/constants";
 
 export default function HeroSection() {
   return (
@@ -15,7 +16,7 @@ export default function HeroSection() {
         <div className="flex flex-col items-center gap-4">
           <img
             className="w-64 h-64 rounded-full object-cover border-4 border-white shadow-2xl transition duration-300 hover:scale-105"
-            src="https://media.licdn.com/dms/image/v2/D4E03AQGUesF_ycFxog/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1710427781734?e=1753315200&v=beta&t=4YdWNtr3Xg5epZ-h2XUV_ta_EIt7cppcjAUjqUKe6lg"
+            src={INNOVATOR_IMG}
             alt="Innovator img"
           />
           <h3 className="text-2xl font-semibold tracking-wide drop-shadow">
